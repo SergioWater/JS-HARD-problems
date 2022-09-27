9f20c16ff8d054b17172f19d722383968e6c35e6
Sergio <sergiof_2001@hotmail.com>
2022-09-27 00:44:24 -0700

    first hard problem

---
+++ app.js
@@ -0,0 +1,16 @@
+function showRating(rating){
+    let ratings = ""
+
+    for(let i = 0; i < Math.floor(rating); i++){
+        ratings = ratings + "*"
+        if(i !== Math.floor(rating) - 1){
+            ratings = ratings + " "
+        }
+    }
+    if(!Number.isInteger(rating)){
+        ratings = ratings + " ."
+    }
+
+    return ratings
+}
+console.log(showRating(1.5))
